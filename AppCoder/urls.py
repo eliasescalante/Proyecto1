65c040212355ff327074c15032daa49026037e20
eliasescalante <eliasw.escalante@gmail.com>
2023-10-05 20:38:25 -0300

    agregue el login y el logout y funciona

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from AppCoder import views
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
@@ -18,6 +19,7 @@ urlpatterns = [
     path('editarProfesor/<profesor_nombre>/', views.editarProfesor,name="EditarProfesor"),
     path('login', views.login_request,name= 'Login'),
     path('register', views.register,name='Register'),
+    path('logout', LogoutView.as_view(template_name='AppCoder/logout.html'),name='Logout'),
 ]
 
 urlpatterns += [
